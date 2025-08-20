060dd4087b51e0244882e8f8dccab4b615141432
aminrezkitaginting06-jpg <aminrezkitaginting06@gmail.com>
2025-08-20 09:35:49 +0700

    Delete reset_bot.py

--- reset_bot.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-
-BOT_TOKEN = "8417540455:AAHowzwxGRwT1BTA5sC6vO1xkBhvMeBry7U"
-bot = Bot(token=BOT_TOKEN)
-
-# Hapus webhook
-bot.delete_webhook()
-print("✅ Webhook dihapus. Bot siap untuk polling.")
